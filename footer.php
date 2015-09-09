cfa500b662f7954d8128c2e2b28852ecaf9fa86a
Nio-av <public@nio-av.de>
2015-09-09 14:31:45 +0200

    Remove Counter

--- footer.php
+++ footer.php
@@ -13,19 +13,22 @@
                     <img src="<?php echo(get_template_directory_uri()); ?>/Banner.svg">
                     <p>                
                           <script type="text/javascript">
-                                  bisZurFeier();
+                                 /* bisZurFeier();
 
                                   function bisZurFeier() {                                                     
                                         jetzt = new Date();
-                                        DM25 = new Date("Jun 12, 2015");
+                                        DM25 = new Date("Jun 13, 2015");
                                         msProTag = 24 * 60 * 60 * 1000 ;
                                         DifferenzTime = (DM25.getTime() - jetzt.getTime());
                                         e_daysLeft = DifferenzTime / msProTag;
                                         TageBis = Math.floor(e_daysLeft); //floor = Abrunden 
 
-                                        document.write("Noch " + TageBis + " Tage bis zur Jubiläumsfeier!" );                        
+ 
+ document.write("Heute " + TageBis + " Tage bis zur Jubiläumsfeier!" );*/                        
+                                      
+                                      document.write("Danke, dass Sie mit dabei waren!" );                        
 
-                                  } 
+                                  /*} */
 
                           </script>             
                     </p> 
@@ -43,7 +46,11 @@
                     <?php wp_nav_menu( array('theme_location' => 'footer-menu' )); ?>
             </nav>
             <aside id="sponsor">
-                Mit freundlicher Unterstützung unseres Platin-Sponsors <img src="<?php echo(get_template_directory_uri()); ?>/Sponsoren/KMS.png" class="sponsor-logo" alt="KMS"></img>
+                <p>
+                Mit freundlicher Unterstützung unseres Platin-Sponsors
+                
+                
+                <img src="<?php echo(get_template_directory_uri()); ?>/Sponsoren/KMS.png" alt="KMS"></img>
             </aside>
             
         </div><!-- ende footer -->
@@ -56,6 +63,5 @@
 <?php
     include_once "tracking.php";
 ?>
-
 </body>
 </html>
